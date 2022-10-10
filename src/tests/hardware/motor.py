f156ddffbbc7578991d9f5931ddea66ba73198ac
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-10-11 00:00:14 +0300

    Fixed the wrong upload

--- src/tests/hardware/motor.py
+++ src/tests/hardware/motor.py
@@ -5,38 +5,21 @@ DIR = 21   # Direction GPIO Pin
 STEP = 20  # Step GPIO Pin
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
-SPR = 8000   # Steps per Revolution (360 / 1.8)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.output(DIR, CW)
 
-MODE = (18, 15, 14)   # Microstep Resolution GPIO Pins
-GPIO.setup(MODE, GPIO.OUT)
-RESOLUTION = {'Full': (0, 0, 0),
-              'Half': (1, 0, 0),
-              '1/4': (0, 1, 0),
-              '1/8': (1, 1, 0),
-              '1/16': (0, 0, 1),
-              '1/32': (1, 0, 1)}
-GPIO.output(MODE, RESOLUTION['Full'])
-
-step_count = SPR
 delay = .0005
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+try:
+	while True:
+	    GPIO.output(STEP, GPIO.HIGH)
+	    sleep(delay)
+	    GPIO.output(STEP, GPIO.LOW)
+	    sleep(delay)
 
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-
-GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print("\nGPIO pins are reset!")
