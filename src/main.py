4c00a724c95ae352895c637d5326e0c31d14d800
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-10-18 00:06:54 +0300

    Updated the source code used for testing

--- src/main.py
+++ src/main.py
@@ -5,9 +5,14 @@ from distance_sensor import Sensor
 from beeper import Beeper
 
 
-motor_1 = Motor(14, 15)
-motor_2 = Motor(23, 24)
-sensor = Sensor(16, 12)
+motor_1 = Motor(15, 14)
+motor_2 = Motor(20, 16)
+
+sensor_1 = Sensor(7, 24)
+sensor_2 = Sensor(25, 8)
+sensor_3 = Sensor(12, 19)
+sensor_4 = Sensor(21, 26)
+
 beeper = Beeper(5)
 
 
@@ -17,19 +22,21 @@ beeper.beep(3, 0.1)
 
 # Motor test
 
-motor_1.rotate(3, 1)
-motor_2.rotate(3, 1)
+motor_1.rotate(5, 1)
+motor_2.rotate(5, 1)
 
 time.sleep(1)
 
-motor_1.rotate(3, 0)
-motor_2.rotate(3, 0)
+motor_1.rotate(5, 0)
+motor_2.rotate(5, 0)
 
 # Distance sensor test
 
-for i in range(10):
-        beeper.beep(1, 0.3)
-        print(sensor.get_distance(), " cm")
+for i in range(30):
+	print("Sensor_1: ", sensor_1.get_distance(), " cm")
+	print("Sensor_2: ", sensor_2.get_distance(), " cm")
+	print("Sensor_3: ", sensor_3.get_distance(), " cm")
+	print("Sensor_4: ", sensor_4.get_distance(), " cm")
 
 # Clear GPIO settings
 
