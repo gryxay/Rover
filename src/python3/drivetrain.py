c941807804d3e163abf0df0f947ad7c8b071c0f6
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-11-22 23:41:26 +0200

    Added the functionality of turning on/off motors

--- src/python3/drivetrain.py
+++ src/python3/drivetrain.py
@@ -8,7 +8,7 @@ TURNING_DELAY = 0.0005	# Time between signal pulses when turning
 
 
 class Drivetrain:
-	def __init__(self, dir_pin_1, step_pin_1, dir_pin_2, step_pin_2):
+	def __init__(self, dir_pin_1, step_pin_1, dir_pin_2, step_pin_2, sleep_pin):
 		# Motor 1
 		self.dir_pin_1 = dir_pin_1
 		self.step_pin_1 = step_pin_1
@@ -16,6 +16,9 @@ class Drivetrain:
 		# Motor 2
 		self.dir_pin_2 = dir_pin_2
 		self.step_pin_2 = step_pin_2
+
+		# Sleep pin
+		self.sleep_pin = sleep_pin
 
 		# GPIO setup
 		GPIO.setmode(GPIO.BCM)
@@ -25,6 +28,7 @@ class Drivetrain:
 		GPIO.setup(self.step_pin_1, GPIO.OUT)
 		GPIO.setup(self.dir_pin_2, GPIO.OUT)
 		GPIO.setup(self.step_pin_2, GPIO.OUT)
+		GPIO.setup(self.sleep_pin, GPIO.OUT)
 
 
 	def rotate(self, direction):
@@ -77,8 +81,12 @@ class Drivetrain:
 			GPIO.output(self.step_pin_2, GPIO.HIGH)
 
 			time.sleep(TURNING_DELAY)
-
+			
 			GPIO.output(self.step_pin_1, GPIO.LOW)
 			GPIO.output(self.step_pin_2, GPIO.LOW)
 			
 			time.sleep(TURNING_DELAY)
+
+
+	def toggle_power(self, is_on):
+		GPIO.output(self.sleep_pin, is_on)
