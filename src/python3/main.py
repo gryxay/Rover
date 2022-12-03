1ae99fd4f6667dec7ae55741ce051644e6fd738b
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-12-03 03:31:06 +0200

    Tidied up the script, fixed basic mapping

--- src/python3/main.py
+++ src/python3/main.py
@@ -1,66 +1,77 @@
 import RPi.GPIO as GPIO
-import time
-import threading
-import math
 from multiprocessing import Process, Value
+import threading
+import time
+import math
 
-from motor import Motor
-from distance_sensor import Sensor
-from beeper import Beeper
 from drivetrain import Drivetrain
-from map_tile import map as Map
+from sensing_system import Sensing_system
+from beeper import Beeper
+
+from map_tile import Map
+
+
+CM = 132 # 132 motor steps = 1cm
 
 
 #beeper = Beeper(5)
-drivetrain = Drivetrain(20, 16, 26, 19)
-front_sensor = Sensor(21, 12)
-left_sensor = Sensor(25, 24)
-right_sensor = Sensor(17, 4)
+drivetrain = Drivetrain()
+sensing_system = Sensing_system()
 
 
-def update_sensor_data(front_sensor_last_scan, left_sensor_last_scan, right_sensor_last_scan):
+def update_sensor_data(sensor_data):
 	while True:
-		front_sensor_last_scan.value = front_sensor.get_distance()
-		left_sensor_last_scan.value = left_sensor.get_distance()
-		right_sensor_last_scan.value = right_sensor.get_distance()
+		sensor_data["front"].value = sensing_system.get_front_sensor_distance()
+		sensor_data["rear"].value = sensing_system.get_rear_sensor_distance()
+		sensor_data["left"].value = sensing_system.get_left_sensor_distance()
+		sensor_data["right"].value = sensing_system.get_right_sensor_distance()
 
 
-def drive(front_sensor_last_scan, left_sensor_last_scan, right_sensor_last_scan, is_moving, map):
-	if front_sensor_last_scan > 10.0:
-		for x in range(math.floor(front_sensor_last_scan/5.0)):		#if 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically)
-			map.add_tile(map.cur_x, map.cur_y + x + 1, obstacle = False)
+def drive(sensor_data, is_moving, map):
+	if sensor_data["front"].value > 10.0:
+		# If 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically). - Aistė
+		for x in range(math.floor(sensor_data["front"].value/5.0)):
+			map.add_tile(map.cur_x, map.cur_y + x + 1, unknown = False)
 	
-	if(front_sensor_last_scan <= 5.0):							# if front last scan <=5 cm - must be osbtacle 
-		map.add_tile(map.cur_x, map.cur_y + 1, obstacle = True)
-		if left_sensor_last_scan.value > right_sensor_last_scan.value:
-			drivetrain.turn('l', 3.15)	# 3.15 ~= 90 degree turn
-		else:
-			drivetrain.turn('r', 3.15)	# 3.15 ~= 90 degree turn
+	step_count = 0
 
-		while front_sensor_last_scan.value > 10.0 and left_sensor_last_scan.value > 5.0 and right_sensor_last_scan.value > 5.0:
-			drivetrain.rotate('f')
-			# somehow every 5cm do map.add_tile(map.cur_x, map.cur_y - 1, visited = True) when we know how much robot has moved 
+	while sensor_data["front"].value >= 10.0 and sensor_data["left"].value > 5.0 and sensor_data["right"].value > 5.0:
+		drivetrain.rotate('f')
+		step_count += 1
+	
+		if step_count == 5 * CM:
+			map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
+			step_count = 0
+
+	map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
+	map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
 
 	map.display_map()
 
+	if sensor_data["left"].value > sensor_data["right"].value:
+		drivetrain.turn('l', 3.15)	# 3.15 ~= 90 degree turn
+	else:
+		drivetrain.turn('r', 3.15)	# 3.15 ~= 90 degree turn
+
 	is_moving.value = 0
 
+
 if __name__ == "__main__":
-	front_sensor_last_scan = Value('f', 11.0)
-	left_sensor_last_scan = Value('f', 7.0)
-	right_sensor_last_scan = Value('f', 7.0)
+	sensor_data = {
+		"front": Value('f', sensing_system.get_front_sensor_distance()),
+		"rear": Value('f', sensing_system.get_rear_sensor_distance()),
+		"left": Value('f', sensing_system.get_left_sensor_distance()),
+		"right": Value('f', sensing_system.get_right_sensor_distance())
+	}
 	is_moving = Value('b', 1)
-	map = Map();
+	map = Map()
 
-	p1 = Process(target=update_sensor_data, args=(front_sensor_last_scan, left_sensor_last_scan, right_sensor_last_scan,))
-	p2 = Process(target=drive, args=(front_sensor_last_scan, left_sensor_last_scan, right_sensor_last_scan, is_moving, map))
+	drivetrain.toggle_power(True)
 
-	p1.start()
-	p2.start()
+	Process(target=update_sensor_data, args=(sensor_data,)).start()
+	Process(target=drive, args=(sensor_data, is_moving, map,)).start()
 	
 	while True:
 		if is_moving.value == 0:
-			p2 = Process(target=drive, args=(front_sensor_last_scan, left_sensor_last_scan, right_sensor_last_scan, is_moving, map))
-			p2.start()
-
+			Process(target=drive, args=(sensor_data, is_moving, map,)).start()
 			is_moving.value = 1
