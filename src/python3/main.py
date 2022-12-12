370620bb44bd4cabbf4a4a27aaaa35fd9a2a409b
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-12-12 15:47:36 +0200

    Removed update_sensor_data() function

--- src/python3/main.py
+++ src/python3/main.py
@@ -14,28 +14,22 @@ from map_tile import Map
 CM = 132 # 132 motor steps = 1cm
 
 
-#beeper = Beeper(5)
+beeper = Beeper()
 drivetrain = Drivetrain()
 sensing_system = Sensing_system()
 
 
-def update_sensor_data(sensor_data):
-	while True:
-		sensor_data["front"].value = sensing_system.get_front_sensor_distance()
-		sensor_data["rear"].value = sensing_system.get_rear_sensor_distance()
-		sensor_data["left"].value = sensing_system.get_left_sensor_distance()
-		sensor_data["right"].value = sensing_system.get_right_sensor_distance()
-
-
-def drive(sensor_data, is_moving, map):
-	if sensor_data["front"].value > 10.0:
+def explore(is_moving, map):
+	if sensing_system.get_front_sensor_distance() > 10.0:
 		# If 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically). - Aistė
-		for x in range(math.floor(sensor_data["front"].value/5.0)):
+		for x in range(math.floor(sensing_system.get_front_sensor_distance() / 5.0)):
 			map.add_tile(map.cur_x, map.cur_y + x + 1, unknown = False)
 	
 	step_count = 0
 
-	while sensor_data["front"].value >= 10.0 and sensor_data["left"].value > 5.0 and sensor_data["right"].value > 5.0:
+	while sensing_system.get_front_sensor_distance() >= 10.0 and sensing_system.get_left_sensor_distance() > 5.0 \
+		and sensing_system.get_right_sensor_distance() > 5.0:
+
 		drivetrain.rotate('f')
 		step_count += 1
 	
@@ -48,30 +42,25 @@ def drive(sensor_data, is_moving, map):
 
 	map.display_map()
 
-	if sensor_data["left"].value > sensor_data["right"].value:
-		drivetrain.turn('l', 3.15)	# 3.15 ~= 90 degree turn
+	if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
+		drivetrain.turn('l', 90)
 	else:
-		drivetrain.turn('r', 3.15)	# 3.15 ~= 90 degree turn
+		drivetrain.turn('r', 90)
 
 	is_moving.value = 0
 
 
 if __name__ == "__main__":
-	sensor_data = {
-		"front": Value('f', sensing_system.get_front_sensor_distance()),
-		"rear": Value('f', sensing_system.get_rear_sensor_distance()),
-		"left": Value('f', sensing_system.get_left_sensor_distance()),
-		"right": Value('f', sensing_system.get_right_sensor_distance())
-	}
 	is_moving = Value('b', 1)
 	map = Map()
 
+	beeper.beep(3, 0.1)
+
 	drivetrain.toggle_power(True)
 
-	Process(target=update_sensor_data, args=(sensor_data,)).start()
-	Process(target=drive, args=(sensor_data, is_moving, map,)).start()
+	Process(target = explore, args = (is_moving, map,)).start()
 	
 	while True:
 		if is_moving.value == 0:
-			Process(target=drive, args=(sensor_data, is_moving, map,)).start()
+			Process(target = explore, args = (is_moving, map,)).start()
 			is_moving.value = 1
