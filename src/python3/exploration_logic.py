5660b6301077ae5a7f71e5890dc9f527a8b169ab
Drungilas Gabrielius <gabrielius.drungilas@mif.stud.vu.lt>
2022-12-13 14:32:49 +0200

    Update exploration_logic.py Adding missing functionality, and deleting unnecessary code

--- src/python3/exploration_logic.py
+++ src/python3/exploration_logic.py
@@ -14,38 +14,7 @@ CM = 132 # 132 motor steps = 1cm
 
 beeper = Beeper()
 drivetrain = Drivetrain()
-sensing_system = Sensing_system()
-
-
-def explore_v1(map):
-	while True:
-		if sensing_system.get_front_sensor_distance() > 10.0:
-			# If 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically). - Aistė
-			for x in range(math.floor(sensing_system.get_front_sensor_distance() / 5.0)):
-				map.add_tile(map.cur_x, map.cur_y + x + 1, unknown = False)
-		
-		step_count = 0
-
-		while sensing_system.get_front_sensor_distance() >= 10.0 and sensing_system.get_left_sensor_distance() > 5.0 \
-			and sensing_system.get_right_sensor_distance() > 5.0:
-
-			drivetrain.rotate('f')
-			step_count += 1
-		
-			if step_count == 5 * CM:
-				map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
-				step_count = 0
-
-		map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
-		map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
-
-		map.display_map()
-
-		if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
-			drivetrain.turn('l', 90)
-		else:
-			drivetrain.turn('r', 90)
-   
+sensing_system = Sensing_system() 
 
 class explore_logic:
     def __init__(self, map_object):
@@ -125,6 +94,16 @@ class explore_logic:
             return map.get_tile(self.map.cur_x + 1, self.map.cur_y).obstacle
         elif self.orientation == 'W':
             return map.get_tile(self.map.cur_x, self.map.cur_y - 1).obstacle
+    
+    def is_wall_on_right(self):
+        if self.orientation == 'N':
+            return map.get_tile(self.map.cur_x + 1, self.map.cur_y).obstacle
+        elif self.orientation == 'E':
+            return map.get_tile(self.map.cur_x, self.map.cur_y - 1).obstacle
+        elif self.orientation == 'S':
+            return map.get_tile(self.map.cur_x - 1, self.map.cur_y).obstacle
+        elif self.orientation == 'W':
+            return map.get_tile(self.map.cur_x, self.map.cur_y + 1).obstacle
 
     def update_map(self):
         if self.orientation == 'N':
@@ -229,74 +208,6 @@ def explore_v3(map):
     while True:
         explore_log.explore()
     
-def explore_v2(map):
-	orientation = 'N'
-
-	while True:
-		step_count = 0
-
-		while sensing_system.get_front_sensor_distance() >= 2 * TILE_SIZE and sensing_system.get_left_sensor_distance() > TILE_SIZE \
-			and sensing_system.get_right_sensor_distance() > TILE_SIZE:
-
-			drivetrain.rotate('f')
-			step_count += 1
-		
-			if step_count == TILE_SIZE * CM:
-				if orientation == 'N':
-					map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
-					map.cur_y += 1
-
-				elif orientation == 'E':
-					map.add_tile(map.cur_x + 1, map.cur_y, unknown = False, visited = True)
-					map.cur_x += 1
-
-				elif orientation == 'S':
-					map.add_tile(map.cur_x, map.cur_y - 1, unknown = False, visited = True)
-					map.cur_y -= 1
-
-				elif orientation == 'W':
-					map.add_tile(map.cur_x - 1, map.cur_y, unknown = False, visited = True)
-					map.cur_x -= 1
-
-				step_count = 0
-
-		map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
-		map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
-
-		if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
-			drivetrain.turn('l', 90)
-
-			if orientation == 'N':
-				orientation = 'W'
-
-			elif orientation == 'W':
-				orientation = 'S'
-
-			elif orientation == 'S':
-				orientation = 'E'
-
-			elif orientation == 'E':
-				orientation = 'N'
-
-		else:
-			drivetrain.turn('r', 90)
-
-			if orientation == 'N':
-				orientation = 'E'
-
-			elif orientation == 'E':
-				orientation = 'S'
-
-			elif orientation == 'S':
-				orientation = 'W'
-
-			elif orientation == 'W':
-				orientation = 'N'
-
-		map.display_map()
-		print("----------------------------------------------------------------------------------------------------")
-
-
 if __name__ == "__main__":
 	map = Map()
 
