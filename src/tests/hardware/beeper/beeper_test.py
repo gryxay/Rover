43598c362017e486335f02fe0736613f3a74bed8
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-12-14 23:51:31 +0200

    Uploaded a beeper test

--- src/tests/hardware/beeper/beeper_test.py
+++ src/tests/hardware/beeper/beeper_test.py
@@ -1,19 +1,9 @@
-import RPi.GPIO as GPIO
-import time
+from beeper import beeper
 
 
-beeper_gpio_pin = 17
-beeps = 10
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(beeper_gpio_pin, GPIO.OUT)
-
-for i in range(beeps):
-	GPIO.output(beeper_gpio_pin, GPIO.LOW)
-	time.sleep(0.5)
-
-	GPIO.output(beeper_gpio_pin, GPIO.HIGH)
-	time.sleep(0.5)
+beeper = Beeper()
 
 
-GPIO.cleanup()
+if __name__ == "__main__":
+	# Beep 3 times with 0.1 second intervals
+	beeper.beep(3, 0.1)
