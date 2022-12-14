6d4ad4193145e03ada34f0fba809fd0547d5ecc2
neja7994 <nedas.janusauskas@mif.stud.vu.lt>
2022-12-14 23:29:31 +0200

    Uploaded a distance sensor test

--- src/tests/hardware/distance_sensor/distance_sensor_test.py
+++ src/tests/hardware/distance_sensor/distance_sensor_test.py
@@ -1,35 +1,19 @@
-import RPi.GPIO as GPIO
-import time
+from time import sleep
+
+from distance_sensor import Distance_sensor
 
 
-trigger_gpio_pin = 14
-echo_gpio_pin = 15
+TRIG_PIN = 24
+ECHO_PIN = 23
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(trigger_gpio_pin, GPIO.OUT)
-GPIO.setup(echo_gpio_pin, GPIO.IN)
+sensor = Distance_sensor(24, 23)
 
-try:
-	while True:
-		GPIO.output(trigger_gpio_pin, GPIO.LOW)
-		time.sleep(0.5)
-		GPIO.output(trigger_gpio_pin, GPIO.HIGH)
-		time.sleep(0.00001)
-		GPIO.output(trigger_gpio_pin, GPIO.LOW)
 
-		while GPIO.input(echo_gpio_pin) == 0:
-			pulse_start = time.time()
+if __name__ == "__main__":
+	try:
+		while True:
+			print(sensor.get_distance())
 
-		while GPIO.input(echo_gpio_pin) == 1:
-			pulse_end = time.time()
-
-		pulse_duration = pulse_end - pulse_start
-		distance = pulse_duration * 17150
-		distance = round(distance, 2)
-
-		print("Distance: ", distance, "cm")
-
-except KeyboardInterrupt:
-	GPIO.cleanup()
-	print("\nGPIO pins are reset!")
+	except KeyboardInterrupt:
+			print("End of the test")
