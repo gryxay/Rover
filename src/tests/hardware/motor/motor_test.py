a01bbb819db72fa4cce8dd1ff14b1518ff58f8e0
Janušauskas Nedas <nedas.janusauskas@mif.stud.vu.lt>
2022-12-14 19:15:03 +0200

    Updated the motor test

--- src/tests/hardware/motor/motor_test.py
+++ src/tests/hardware/motor/motor_test.py
@@ -1,25 +1,46 @@
-from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 21   # Direction GPIO Pin
-STEP = 20  # Step GPIO Pin
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
+from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
 
-delay = .0005
+DIR_PIN = 16   	# Direction GPIO Pin
+STEP_PIN = 20	# Step GPIO Pin
+CW = 1     	# Clockwise Rotation
+CCW = 0    	# Counterclockwise Rotation
+SPR = 800	# Signal pulses per revolution
 
-try:
-	while True:
-		GPIO.output(STEP, GPIO.HIGH)
-		sleep(delay)
-		GPIO.output(STEP, GPIO.LOW)
-		sleep(delay)
+SLOW_TEST_DELAY = .001	# Time between signal pulses
+SLOW_TEST_ROTATIONS = 3	# Times that the motor shaft turns
 
-except KeyboardInterrupt:
-	GPIO.cleanup()
-	print("\nGPIO pins are reset!")
+FAST_TEST_DELAY = .001	# Time between signal pulses
+FAST_TEST_ROTATIONS = 50	# Times that the motor shaft turns
+
+
+def rotate(direction, rotations, delay):
+		GPIO.output(DIR_PIN, direction)
+	
+		for pulse in range(rotations * SPR):
+			GPIO.output(STEP_PIN, GPIO.HIGH)
+			sleep(delay)
+
+			GPIO.output(STEP_PIN, GPIO.LOW)
+			sleep(delay)
+
+
+if __name__ == "__main__":
+	# GPIO setup
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(DIR, GPIO.OUT)
+	GPIO.setup(STEP, GPIO.OUT)
+
+	# Motor test 1. Clockwise direction. Low speed.
+	rotate(CW, SLOW_TEST_ROTATIONS, SLOW_TEST_DELAY)
+
+	# Motor test 2. Counterclockwise direction. Low speed.
+	rotate(CCW, SLOW_TEST_ROTATIONS, SLOW_TEST_DELAY)
+
+	# Motor test 3. Clockwise direction. High speed.
+	rotate(CW, FAST_TEST_ROTATIONS, FAST_TEST_DELAY)
+
+	# Motor test 4. Counterclockwise direction. High speed.
+	rotate(CCW, FAST_TEST_ROTATIONS, FAST_TEST_DELAY)
