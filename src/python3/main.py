e64cb591d8c597251be7be25edf7b790852edac0
Grigaliūnaitė Aistė <aiste.grigaliunaite@mif.stud.vu.lt>
2022-12-18 20:49:38 +0200

    Refactor main.py

--- src/python3/main.py
+++ src/python3/main.py
@@ -18,107 +18,95 @@ sensing_system = Sensing_system()
 
 
 def explore_v1(map):
-	while True:
-		if sensing_system.get_front_sensor_distance() > 10.0:
-			# If 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically). - Aistė
-			for x in range(math.floor(sensing_system.get_front_sensor_distance() / 5.0)):
-				map.add_tile(map.cur_x, map.cur_y + x + 1, unknown = False)
-		
-		step_count = 0
+    while True:
+        if sensing_system.get_front_sensor_distance() > 10.0:
+            # If 33cm in front is free, 33/5 = 6 tiles of free non-obstacle tiles are added to map (theoretically). - Aistė
+            for x in range(math.floor(sensing_system.get_front_sensor_distance() / 5.0)):
+                map.add_tile(map.cur_x, map.cur_y + x + 1, unknown = False)
+        
+        step_count = 0
 
-		while sensing_system.get_front_sensor_distance() >= 10.0 and sensing_system.get_left_sensor_distance() > 5.0 \
-			and sensing_system.get_right_sensor_distance() > 5.0:
+        while sensing_system.get_front_sensor_distance() >= 10.0 and sensing_system.get_left_sensor_distance() > 5.0 \
+            and sensing_system.get_right_sensor_distance() > 5.0:
 
-			drivetrain.rotate('f')
-			step_count += 1
-		
-			if step_count == 5 * CM:
-				map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
-				step_count = 0
+            drivetrain.rotate('f')
+            step_count += 1
+        
+            if step_count == 5 * CM:
+                map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
+                step_count = 0
 
-		map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
-		map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
+        map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
+        map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
 
-		map.display_map()
+        map.display_map()
 
-		if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
-			drivetrain.turn('l', 90)
-		else:
-			drivetrain.turn('r', 90)
+        if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
+            drivetrain.turn('l', 90)
+        else:
+            drivetrain.turn('r', 90)
    
 
 def explore_v2(map):
-	orientation = 'N'
+    orientations = ['N', 'E', 'S', 'W']
+    orientation = orientations[0]
 
-	while True:
-		step_count = 0
+    while True:
+        step_count = 0
 
-		while sensing_system.get_front_sensor_distance() >= 2 * TILE_SIZE and sensing_system.get_left_sensor_distance() > TILE_SIZE \
-			and sensing_system.get_right_sensor_distance() > TILE_SIZE:
+        while sensing_system.get_front_sensor_distance() >= 2 * TILE_SIZE and sensing_system.get_left_sensor_distance() > TILE_SIZE \
+            and sensing_system.get_right_sensor_distance() > TILE_SIZE:
 
-			drivetrain.rotate('f')
-			step_count += 1
-		
-			if step_count == TILE_SIZE * CM:
-				if orientation == 'N':
-					map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
-					map.cur_y += 1
+            drivetrain.rotate('f')
+            step_count += 1
+        
+            if step_count == TILE_SIZE * CM:
+                if orientation == 'N':
+                    map.add_tile(map.cur_x, map.cur_y + 1, unknown = False, visited = True)
+                    map.cur_y += 1
 
-				elif orientation == 'E':
-					map.add_tile(map.cur_x + 1, map.cur_y, unknown = False, visited = True)
-					map.cur_x += 1
+                elif orientation == 'E':
+                    map.add_tile(map.cur_x + 1, map.cur_y, unknown = False, visited = True)
+                    map.cur_x += 1
 
-				elif orientation == 'S':
-					map.add_tile(map.cur_x, map.cur_y - 1, unknown = False, visited = True)
-					map.cur_y -= 1
+                elif orientation == 'S':
+                    map.add_tile(map.cur_x, map.cur_y - 1, unknown = False, visited = True)
+                    map.cur_y -= 1
 
-				elif orientation == 'W':
-					map.add_tile(map.cur_x - 1, map.cur_y, unknown = False, visited = True)
-					map.cur_x -= 1
+                elif orientation == 'W':
+                    map.add_tile(map.cur_x - 1, map.cur_y, unknown = False, visited = True)
+                    map.cur_x -= 1
 
-				step_count = 0
+                step_count = 0
 
-		map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
-		map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
+        map.add_tile(map.cur_x, map.cur_y + 1, unknown = False)
+        map.add_tile(map.cur_x, map.cur_y + 2, unknown = False, obstacle = True)
 
-		if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
-			drivetrain.turn('l', 90)
+        if sensing_system.get_left_sensor_distance() > sensing_system.get_right_sensor_distance():
+            drivetrain.turn('l', 90)
 
-			if orientation == 'N':
-				orientation = 'W'
+            if orientation == 'N':
+                orientation = 'W'
+            
+            else:
+                orientation = orientations[orientations.index(orientation) - 1]
 
-			elif orientation == 'W':
-				orientation = 'S'
+        else:
+            drivetrain.turn('r', 90)
 
-			elif orientation == 'S':
-				orientation = 'E'
+            if orientation == 'W':
+                orientation = 'N'
+            
+            else:
+                orientation = orientations[orientations.index(orientation) + 1]
 
-			elif orientation == 'E':
-				orientation = 'N'
-
-		else:
-			drivetrain.turn('r', 90)
-
-			if orientation == 'N':
-				orientation = 'E'
-
-			elif orientation == 'E':
-				orientation = 'S'
-
-			elif orientation == 'S':
-				orientation = 'W'
-
-			elif orientation == 'W':
-				orientation = 'N'
-
-		map.display_map()
-		print("----------------------------------------------------------------------------------------------------")
+        map.display_map()
+        print("----------------------------------------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
-	map = Map()
+    map = Map()
 
-	beeper.beep(3, 0.1)
+    beeper.beep(3, 0.1)
 
-	Process(target = explore_v2, args = (map,)).start()
-	
+    Process(target = explore_v2, args = (map,)).start()
