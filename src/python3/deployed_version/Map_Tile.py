6273ffda1c29b4cd662a87f1affb1a611a43d2af
Janušauskas Nedas <nedas.janusauskas@mif.stud.vu.lt>
2022-12-28 05:24:51 +0200

    Uploaded a deployed version of Map_Tile.py

---
+++ src/python3/deployed_version/Map_Tile.py
@@ -0,0 +1,45 @@
+class Map_Tile:
+    __x = None
+    __y = None
+
+    __times_visited = None
+    __distance = None
+
+    is_known = None
+    is_obstacle = None
+
+
+    def __init__(self, x, y, times_visited = 0, is_known = False, is_obstacle = False):
+        self.__x = x
+        self.__y = y
+        self.__times_visited = times_visited
+        self.is_known = is_known
+        self.is_obstacle = is_obstacle
+    
+
+    def get_x_position(self) -> int:
+        return self.__x
+
+
+    def get_y_position(self) -> int:
+        return self.__y
+
+
+    def get_position(self) -> tuple:
+        return (self.__x, self.__y)
+
+
+    def increment_times_visited(self):
+        self.__times_visited += 1
+
+
+    def get_times_visited(self) -> int:
+        return self.__times_visited
+
+
+    def set_distance(self, distance):
+        self.__distance = distance
+
+
+    def get_distance(self) -> int:
+        return self.__distance
